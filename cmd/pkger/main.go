83d4d3145a71394312b1de32577b6b6a53b7498b
Joseph Austin <joseph.the.austin@gmail.com>
2021-03-11 12:22:28 -0800

    Bypass safety

--- cmd/pkger/main.go
+++ cmd/pkger/main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/ambitvpn/pkger/cmd/pkger/cmds"
 )
 
 func main() {
-	defer clean()
-
-	defer func() {
-		if err := recover(); err != nil {
-			clean()
-			log.Fatal(err)
-		}
-	}()
-
-	if err := run(); err != nil {
-		clean()
-		log.Fatal(err)
-	}
-}
-
-func run() error {
 	root, err := cmds.New()
 	if err != nil {
-		return err
+		root.Route(os.Args[1:])
 	}
-
-	return root.Route(os.Args[1:])
 }
-
-// does not compute
